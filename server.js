1b5010b3cb9478d519eb07cb20075fbbbb118d66
Blake Spencer <blake.spencer1@gmail.com>
2018-10-08 17:21:48 -0400

    [feature] set up express to serve client html

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema.js');
 const db = require('./db');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
-//all requests go through here
+app.use(morgan('dev'));
+
 app.use(
   '/graphql',
   expressGraphQL({
@@ -13,6 +16,18 @@ app.use(
     graphiql: true,
   })
 );
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
 
 db.sync();
 
